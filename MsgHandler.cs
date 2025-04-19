e9df7a4e283efb9226c0fcbf2b3e9b557433a17b
AlanYuc <alan.yuc@outlook.com>
2025-04-19 21:30:34 +0800

    添加客户端发送消息的测试

--- MsgHandler.cs
+++ MsgHandler.cs
@@ -15,4 +15,19 @@ public class MsgHandler
         MsgPong msgPong = new MsgPong();
         NetManager.Send(clientState, msgPong);
     }
+
+    /// <summary>
+    /// 测试客户端发消息
+    /// </summary>
+    /// <param name="clientState"></param>
+    /// <param name="msgBase"></param>
+    public static void MsgTest(ClientState clientState, MsgBase msgBase)
+    {
+        Console.WriteLine("MsgPing");
+        clientState.lastPingTime = NetManager.GetTimeStamp();
+
+        //给客户端发送Pong
+        MsgPong msgPong = new MsgPong();
+        NetManager.Send(clientState, msgPong);
+    }
 }
