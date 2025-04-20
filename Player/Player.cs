8f57223c803975633936d1a30ae8aacf57c8e911
AlanYuc <alan.yuc@outlook.com>
2025-04-20 21:48:23 +0800

    添加玩家管理类

---
+++ Player/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable disable
+
+public class Player
+{
+    /// <summary>
+    /// 玩家ID
+    /// </summary>
+    public string id = "";
+    /// <summary>
+    /// 玩家所在的客户端
+    /// </summary>
+    public ClientState clientState;
+    /// <summary>
+    /// 玩家的数据
+    /// </summary>
+    public PlayerData playerData;
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="clientState">对应的客户端</param>
+    public Player(ClientState clientState)
+    {
+        this.clientState = clientState;
+    }
+
+    /// <summary>
+    /// 封装好的玩家的发送消息
+    /// </summary>
+    /// <param name="msgBase">要发送的消息</param>
+    public void Send(MsgBase msgBase)
+    {
+        NetManager.Send(clientState, msgBase);
+    }
+}
---
+++ Player/PlayerManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable disable
+
+public class PlayerManager
+{
+    /// <summary>
+    /// 在线玩家字典
+    /// </summary>
+    public static Dictionary<string, Player> players = new Dictionary<string, Player>();
+
+    /// <summary>
+    /// 判断玩家是否在线
+    /// </summary>
+    /// <param name="id">需要判断是否在线的玩家id</param>
+    /// <returns></returns>
+    public static bool IsOnline(string id)
+    {
+        return players.ContainsKey(id);
+    }
+
+    /// <summary>
+    /// 根据id获取对应的玩家
+    /// </summary>
+    /// <param name="id">玩家id</param>
+    /// <returns></returns>
+    public static Player GetPlayer(string id)
+    {
+        if (players.ContainsKey(id))
+        {
+            return players[id];
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// 增加玩家
+    /// </summary>
+    /// <param name="id">玩家id</param>
+    /// <param name="player">玩家</param>
+    public static void AddPlayer(string id,Player player)
+    {
+        players.Add(id, player);
+    }
+
+    /// <summary>
+    /// 删除玩家
+    /// </summary>
+    /// <param name="id">玩家id</param>
+    public static void RemovePlayer(string id)
+    {
+        players.Remove(id);
+    }
+}
