8060baa98c32eedef15065b63fde0bfedf7bc7e4
AlanYuc <alan.yuc@outlook.com>
2025-05-20 21:48:51 +0800

    添加进行消息传递的欢乐豆的类

---
+++ Player/PlayerBeansInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 玩家欢乐豆的信息
+/// </summary>
+[Serializable]
+public class PlayerBeansInfo
+{
+    /// <summary>
+    /// 玩家id
+    /// </summary>
+    public string playerId;
+    /// <summary>
+    /// 该玩家增加或减少的欢乐斗
+    /// </summary>
+    public int beansDelta;
+    /// <summary>
+    /// 对局的倍数
+    /// </summary>
+    public int multiplier = 1;
+}
