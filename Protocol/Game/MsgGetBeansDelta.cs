e9147a030258e401a052e403c30cfd1b1b5bc723
AlanYuc <alan.yuc@outlook.com>
2025-05-20 21:49:09 +0800

    添加获取结算时欢乐豆变化的消息

---
+++ Protocol/Game/MsgGetBeansDelta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MsgGetBeansDelta : MsgBase
+{
+    /// <summary>
+    /// 获取所有玩家及其欢乐豆变化
+    /// </summary>
+    public PlayerBeansInfo[] playerBeansInfos = new PlayerBeansInfo[3];
+
+    public MsgGetBeansDelta()
+    {
+        protocolName = "MsgGetBeansDelta";
+    }
+}
